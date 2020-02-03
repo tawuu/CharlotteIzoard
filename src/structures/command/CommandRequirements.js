b4c0e6c545d9a56d1f9719d039d9d41cd51e043c
ItzNerd <richardmartinsrsms@gmail.com>
2020-02-03 00:12:46 -0300

    Update CommandRequirements.js

--- src/structures/command/CommandRequirements.js
+++ src/structures/command/CommandRequirements.js
@@ -11,16 +11,22 @@ const parseOptions = function (options = {}) {
 const handleRequirements = function ({ author, channel, client, guild, member, me, t, dbBot }, options) {
 	let opt = parseOptions(options);
 
-	if (opt.onlyGuild && channel.type !== "dm") return throw new Error(t('permissions:guildOnly'));
-
-	if (opt.onlyDevs && !dbBot.staffers.owners.includes(author.id)) return throw new Error(t('permissions:onlyDevelopers'));
-
-	if (opt.botPermissions && !me.hasPermission(opt.botPermissions)) return throw new Error(t('permissions:meWithoutPermission', {
-		perms: opt.botPermissions.map(a => new Permissions(a).toArray()[0]).join(', ')
-	}));
-	if (opt.permissions && !member.hasPermission(opt.permissions)) return throw new Error(t('permissions:missingPermissions', {
-		perms: opt.permissions.map(a => new Permissions(a).toArray()[0]).join(', ')
-	}))
+	if (opt.onlyGuild && !guild) {
+		throw new Error(t('permissions:guildOnly'));
+	}
+	if (opt.onlyDevs && !dbBot.staffers.owners.includes(author.id)){
+		throw new Error(t('permissions:onlyDevelopers'));
+	}
+	if (opt.botPermissions && !me.hasPermission(opt.botPermissions)) {
+		throw new Error(t('permissions:meWithoutPermission', {
+			perms: opt.botPermissions.map(a => new Permissions(a).toArray()[0]).join(', ')
+		}));
+	}
+	if (opt.permissions && !member.hasPermission(opt.permissions)) {
+		throw new Error(t('permissions:missingPermissions', {
+			perms: opt.permissions.map(a => new Permissions(a).toArray()[0]).join(', ')
+		}))
+	}
 }
 module.exports = {
 	handleRequirements
