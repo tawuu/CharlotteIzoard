855bc8674494d00ce568a65c9b301e4f32d5d257
ItzNerd <richardmartinsrsms@gmail.com>
2020-02-06 15:20:28 -0300

    terminando o comando de spotify

--- src/commands/utils/SpotifyCommand.js
+++ src/commands/utils/SpotifyCommand.js
@@ -1,6 +1,7 @@
 const CommandHandler = require('../../structures/command/CommandHandler');
 const Canvas = require("canvas");
 const {MessageAttachment} = require("discord.js")
+const moment = require("moment")
 
 module.exports = class SpotifyCommand extends CommandHandler {
     constructor(client) {
@@ -11,35 +12,59 @@ module.exports = class SpotifyCommand extends CommandHandler {
             requirements: {}
         })
     }
-    async execute({ guild, member, channel, prefix, author, t, CharlotteEmbed, dbBot, getUserAt }, args) {
+    async execute({ guild, member, reply, channel, prefix, author, t, CharlotteEmbed, dbBot, getUserAt }, args) {
         
         let user = await getUserAt(0) || author
         let activity = user.presence
         let spotify = activity.activities.filter(a => a.name === "Spotify")
 
-        if (activity && spotify && spotify[0].name.toLowerCase().includes("spotify")) {
-
+        if (activity && spotify.length >= 1 && spotify[0].name.toLowerCase().includes("spotify")) {
+            
             const canvas = Canvas.createCanvas(512 * 3, 512);
             const ctx = canvas.getContext("2d");
             let nome = spotify[0].details
             let by = spotify[0].state
-                
+            
             let img = await Canvas.loadImage(spotify[0].assets.largeImageURL());
             let spotifyLogo = await Canvas.loadImage("https://cdn.discordapp.com/emojis/554334875411415107.png");
-            ctx.drawImage(img, 0, 0, 512, 512);
             ctx.drawImageBlurred(img, 10, 512, -512 / 2, 512 * 2, 512 * 2);
-            ctx.drawImage(spotifyLogo, 550, 38, 100, 100)
             ctx.fillStyle = "white"
-            ctx.font = "60px Impact"
             
-            ctx.fillText(nome, 550, 200, (512*3)-550)
-            ctx.fillText(by, 550, 280, (512*3)-550)
-            ctx.fillStyle = "#1DB954"
-            //val - ((val / 100) * porcent)
-            ctx.roundRect(550, 512 - 35, ((512 * 3) - 600), 20, 10, true, false)//linha preenchida
+            ctx.shadowOffsetX = 0
+            ctx.shadowOffsetY = -200
+            ctx.shadowColor = "black"
+            ctx.shadowBlur = 300
+            ctx.fillRect(0, 512, canvas.width + 200, 5000)
+            
 
-            ctx.roundRect(550, 512 - 35, (512 * 3) - 600, 20, 10, false, true)
-            channel.send(new MessageAttachment(canvas.toBuffer(), "test.png"));
+            
+
+
+            ctx.drawImage(img, 0, 0, 512, 512);
+            ctx.drawImage(spotifyLogo, 550, 38, 100, 100)
+            let start = spotify[0].timestamps.start
+            let end = spotify[0].timestamps.end
+            let valor = (new Date() - start) / (end - start)
+            
+            ctx.font = "60px Arial Black"
+            ctx.fillText(nome, 550, 340, (512 * 3) - 550)
+            ctx.font = "50px Arial Black"
+        
+            ctx.fillText(by, 550, 400, (512 * 3) - 550)
+            ctx.font = "40px Arial Black"
+            ctx.fillText(moment.utc(new Date - start).format("mm:ss"), 550, 512 - 50)
+            ctx.fillText(moment.utc(end - start).format("mm:ss"), canvas.width - 175, 512 - 50)
+            
+            //barra
+            ctx.fillStyle = "#737373"
+            ctx.roundRect(550, 512 - 35, (512 * 3) - 600, 20, 10, true, false)
+            
+
+            ctx.fillStyle = "#1DB954"             
+            ctx.roundRect(550, 512 - 35, valor * ((512 * 3) - 600), 20, 10, true, false)//linha preenchida
+
+
+            channel.send(new MessageAttachment(canvas.toBuffer(), "spotify.png"));
 
         } else {
             reply("notListening")
