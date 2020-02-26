c86a6868b9252e73ae944961fb26c74a5dd0883a
Richard <richardmartinsrsms@gmail.com>
2020-02-26 11:32:09 -0300

    Updates nos loaders

--- src/loaders/LocaleLoader.js
+++ src/loaders/LocaleLoader.js
@@ -1,46 +1,43 @@
-const { readdirSync, createWriteStream } = require('fs');
-const fs = require('fs');
-const http = require('https');
-const chalk = require('chalk')
-const i18next = require('i18next');
-const translationBackend = require('i18next-node-fs-backend');
+const {readdirSync, createWriteStream} = require("fs");
+const fs = require("fs");
+const http = require("https");
+const chalk = require("chalk");
+const i18next = require("i18next");
+const translationBackend = require("i18next-node-fs-backend");
 
-module.exports = class EventLoader  {
-    constructor(client) {
-        this.client = client
-        this.languages = ["pt-BR", "en-US"]
-        this.ns = ['commands', 'events', 'permissions', 'help']
-    }
+module.exports = class EventLoader {
+	constructor(client) {
+		this.client = client;
+		this.languages = ["pt-BR", "en-US"];
+		this.ns = ["commands", "events", "permissions", "help"];
+	}
 
+	load() {
+		try {
+			console.log(chalk.green("Locales are initializing"));
+			this.initializeLocales();
+			return true;
+		} catch (err) {
+			console.error(err);
+		}
+	}
 
-    load() {
-        try {
-            console.log(chalk.green("Locales are initializing"))
-            this.initializeLocales()
-            return true
-        } catch (err) {
-            console.error(err)
-        }
-    }
-
-    async initializeLocales () {
-        try {
-            i18next.use(translationBackend).init({
-                ns: this.ns,
-                preload: await readdirSync('./src/locales/'),
-                fallbackLng: 'pt-BR',
-                backend: {
-                    loadPath: `src/locales/{{lng}}/{{ns}}.json`
-                },
-                interpolation: {
-                    escapeValue: false
-                },
-                returnEmptyString: false
-            })
-
-        } catch(err) {
-            console.error(err)
-        }
-    }
-
-}
+	async initializeLocales() {
+		try {
+			i18next.use(translationBackend).init({
+				ns: this.ns,
+				preload: await readdirSync("./src/locales/"),
+				fallbackLng: "pt-BR",
+				backend: {
+					loadPath: `src/locales/{{lng}}/{{ns}}.json`
+				},
+				interpolation: {
+					escapeValue: false
+				},
+				returnEmptyString: false
+			});
+		} catch (err) {
+			console.error(err);
+		}
+	}
+};
