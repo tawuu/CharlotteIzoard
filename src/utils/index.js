30ff91d3001a187ba5e0bcbf7f10c16d4be23ebb
Richard <richardmartinsrsms@gmail.com>
2020-02-26 11:35:05 -0300

    update nos utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,9 @@
 module.exports = {
     CanvasUtils: require("./Canvas/CanvasUtils"),
     CanvasTemplates: require("./Canvas/Templates"),
+
+    Permissions: require("./Permissions"),
+
+    MessageCollector: require("./ErisUtils/MessageCollector"),
+    ReactionCollector: require("./ErisUtils/ReactionCollector")
 }
