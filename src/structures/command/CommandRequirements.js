804643c0bcc07d3107957d7862ce1beba6f6c856
Richard <richardmartinsrsms@gmail.com>
2020-02-26 11:35:26 -0300

    Update na forma de como checar as permissões

--- src/structures/command/CommandRequirements.js
+++ src/structures/command/CommandRequirements.js
@@ -1,4 +1,4 @@
-const { Permissions } = require('discord.js');
+const { Constants } = require("eris")
 
 const parseOptions = function (options = {}) {
 	return {
@@ -12,24 +12,20 @@ const parseOptions = function (options = {}) {
 const handleRequirements = function ({ author, channel, client, guild, member, me, t, dbBot }, options) {
 	let opt = parseOptions(options);
 
-	if (!me.permissions.has("SEND_MESSAGES")) throw new Error(t("permissions:meWithoutPermission", {
-		perms: `SEND_MESSAGES`
-	}))
-
 	if (opt.onlyDevs && !dbBot.staffers.owners.includes(author.id)){
 		throw new Error(t('permissions:onlyDevelopers'));
 	}
 
 	if (opt.botPermissions && !opt.botPermissions.lenght > 0) {
-		if (!channel.permissionsFor(me).has(opt.botPermissions)) throw new Error(t('permissions:meWithoutPermission', {
-				perms: opt.botPermissions.map(a => new Permissions(a).toArray()[0]).join(', ')
+		if (!client.utils.Permissions.parseChannelPermissionFor(opt.botPermissions, me, channel)) throw new Error(t('permissions:meWithoutPermission', {
+				perms: opt.botPermissions.map(a => client.utils.Permissions.getKeyByValue(Constants.Permissions, a)).join(', ')
 			}));
-		
+
 	}
 	if (opt.permissions && !opt.permissions.lenght > 0) {
-		if (!channel.permissionsFor(member).has(opt.permissions)) {
+		if (!client.utils.Permissions.parseChannelPermissionFor(opt.permissions, member, channel)) {
 			throw new Error(t('permissions:missingPermissions', {
-				perms: opt.permissions.map(a => new Permissions(a).toArray()[0]).join(', ')
+				perms: opt.permissions.map(a => client.utils.Permissions.getKeyByValue(Constants.Permissions, a)).join(', ')
 			}))
 		}
 	}
