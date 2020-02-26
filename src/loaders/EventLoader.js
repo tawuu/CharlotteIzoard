5cdb06886b378048719304d7e2e068047e7e8544
Richard <richardmartinsrsms@gmail.com>
2020-02-26 11:31:02 -0300

    Trocar a collection do Djs para o do Eris

--- src/loaders/EventLoader.js
+++ src/loaders/EventLoader.js
@@ -1,36 +1,35 @@
-const { Collection } = require('discord.js');
-const { readdirSync } = require('fs');
-const chalk = require('chalk')
+const {Collection} = require("eris");
+const {readdirSync} = require("fs");
+const chalk = require("chalk");
 
-module.exports = class EventLoader  {
-    constructor(client) {
-        this.client = client
-    }
-    
-    load () {
-        try {
-            console.log(chalk.green("Events are initializing"))
-            this.initializeEvents()
-            return true
-        } catch (err) {
-            console.error(err)
-        }
-    }
+module.exports = class EventLoader {
+	constructor(client) {
+		this.client = client;
+	}
 
-    initializeEvents () {
-        this.client.events = new Collection();
-        let eventsFiles = readdirSync('src/events')
-        for (let file of eventsFiles) {
-            const event = new (require('../events/' + file))(this.client)
-            event.dir = `/events/${file}`
-            this.client.events.set(event.name, event)
-        }
-        this.addListener()
-    }
-    addListener() {
+	load() {
+		try {
+			console.log(chalk.green("Events are initializing"));
+			this.initializeEvents();
+			return true;
+		} catch (err) {
+			console.error(err);
+		}
+	}
 
-        this.client.events.forEach(event => {
-            this.client.on(event.name, (...args) => event.run(...args))
-        });
-    }
-}
+	initializeEvents() {
+		this.client.events = new Collection();
+		let eventsFiles = readdirSync("src/events");
+		for (let file of eventsFiles) {
+			const event = new (require("../events/" + file))(this.client);
+			event.dir = `/events/${file}`;
+			this.client.events.set(event.name, event);
+		}
+		this.addListener();
+	}
+	addListener() {
+		this.client.events.forEach(event => {
+			this.client.on(event.name, (...args) => event.run(...args));
+		});
+	}
+};
