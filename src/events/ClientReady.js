a4180355ee1d42178efce27534b376d281569906
Richard <richardmartinsrsms@gmail.com>
2020-02-26 11:33:48 -0300

    atualizar eventos...

--- src/events/ClientReady.js
+++ src/events/ClientReady.js
@@ -7,33 +7,17 @@ module.exports = class ClientOnReady extends EventHandler {
     }
     run () {
         console.log(`A super ${chalk.hex('#ff0ab1')('Charlotte')} está online`)
-        
-        this.client.user.setActivity("Estou Online!!!", {
-            type: "WATCHING"
+
+        this.client.editStatus("dnd", {
+            name: "Esto Online Amsx",
+            type: 3
+
         })
-        
-        // let arr = ['users', 'guilds', 'channels', "emojis"]
-        // for (let a of arr) {
-            //     this.getAll(a)
-            // }
-        
-        
-        this.loadCommandsDatabase()
-    
+
+        // this.loadCommandsDatabase()
+
     }
-    getAll (name) {
-        this.client.shard.broadcastEval('this.' + name)
-            .then(results => {
-                let totalResult = []
-                for (let arrayGuild of results) {
-                    for (let guildList of arrayGuild) {
-                        totalResult.push(guildList)
-                    }
-                }
-        this.client[`all${name.slice(0,1).toUpperCase()+name.slice(1)}`] = totalResult
-      })
-        .catch(console.error);
-    }
+
     async loadCommandsDatabase() {
         if (!this.client.database.ok) return;
         let me = await this.client.database.me.findById(process.env.CLIENT_ID)
--- src/events/MessageReceive.js
+++ src/events/MessageReceive.js
@@ -1,64 +1,66 @@
 const EventHandler = require('../structures/EventHandler');
 const CommandContext = require('../structures/command/CommandContext');
 const i18next = require('i18next');
-const CharlotteEmbed = require("../structures/CharlotteEmbed")
-const { DMChannel } = require("discord.js")
 
 module.exports = class MessageReceive extends EventHandler {
     constructor(client) {
-        super(client, 'message')
+        super(client, 'messageCreate')
     }
-    async run (message) {
+    async run ({content, author, channel, member}) {
+        let guild = channel.guild
         let prefix = this.client.user.username.toLowerCase().endsWith("canary") ? "c-" : '-';
-        if (message.channel instanceof DMChannel) return;
-        if (message.author.bot) return;
+        if (channel.type === 1) return;
+        if (author.bot) return;
 
         let t;
         const setFixedT = function (translate) { t = translate };
         setFixedT(i18next.getFixedT('pt-BR'));
 
-        if (message.content.replace(/!/g, "") === message.guild.me.toString()) {
-            message.reply(t("events:mention", {
+        if (content.replace(/[!<>@]/g, "") === this.client.user.id.toString()) {
+            this.client.createMessage(channel.id, t("events:mention", {
                 prefix
             }))
         }
 
-
-        if (!message.content.startsWith(prefix)) return;
+        if (!content.startsWith(prefix)) return;
         let bot = await this.client.database.me.findById(process.env.client_id);
         if (!bot) {
             bot = new this.client.database.me({
                 _id: process.env.client_id
             }).save()
         }
-        let user = await this.client.database.users.findById(message.author.id);
+        let user = await this.client.database.users.findById(author.id);
         if (!user) {
             user = new this.client.database.users({
-                _id: message.author.id
+                _id: author.id
             }).save()
         }
-        let guild = await this.client.database.guilds.findById(message.guild.id);
-        if (!guild) {
-            guild = new this.client.database.guilds({
-                _id: message.guild.id
+        let guildDB = await this.client.database.guilds.findById(guild.id);
+        if (!guildDB) {
+            guildDB = new this.client.database.guilds({
+                _id: guild.id
             }).save()
         }
 
 
-        let args = message.content.slice(prefix.length).trim().split(/ /g);
+        let args = content.slice(prefix.length).trim().split(/ /g);
         let command = args.shift().toLowerCase();
         let cmd = this.client.commands.get(command) || this.client.commands.get(this.client.alias.get(command));
         if (!cmd) return;
         let context = new CommandContext(this.client, {
-            message,
+            guild,
+            author,
+            channel,
+            member,
+            content,
             prefix,
             t,
             user,
             bot,
-            guild,
-            CharlotteEmbed
+            guildDB,
+            CharlotteEmbed: null
         });
-        
+
         let commandDb = await this.client.database.me.findById(process.env.client_id);
         let com = commandDb.commands.find(a => a._id === cmd.name)
         if (com) {
@@ -68,6 +70,7 @@ module.exports = class MessageReceive extends EventHandler {
             com.uses += 1
             commandDb.save()
         }
+        this.client.sendChannelTyping(channel.id)
         cmd._execute(context, args).catch(err => {
             context.reply("Não consegui fazer a finalização deste comando: ```js\n"+err.message+"```")
         })
