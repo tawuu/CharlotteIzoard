2b0ba403e114d430719a31b4c274ba81ebdaeda4
Richard <richardmartinsrsms@gmail.com>
2020-02-26 11:32:22 -0300

    Update nos loaders

--- src/loaders/DatabaseLoader.js
+++ src/loaders/DatabaseLoader.js
@@ -1,36 +1,35 @@
-const { Collection } = require('discord.js');
-const { readdirSync } = require('fs');
-const chalk = require('chalk');
-const { MongoDB } = require('../database');
+const {Collection} = require("eris");
+const {readdirSync} = require("fs");
+const chalk = require("chalk");
+const {MongoDB} = require("../database");
 
-module.exports = class DatabaseLoader  {
-    constructor(client) {
-        this.client = client;
+module.exports = class DatabaseLoader {
+	constructor(client) {
+		this.client = client;
+	}
 
-    }
+	load() {
+		try {
+			console.log(chalk.green("Database are initializing"));
+			this.initializeDatabase(MongoDB);
+			return true;
+		} catch (err) {
+			console.error(err);
+		}
+	}
 
-    load () {
-        try {
-            console.log(chalk.green("Database are initializing"));
-            this.initializeDatabase(MongoDB);
-            return true
-        } catch (err) {
-            console.error(err)
-        }
-    }
-
-    initializeDatabase (DBWrapper, options = {}) {
-        this.client.database = new DBWrapper(options);
-        this.client.database.ok = false
-        this.client.database.startConnection()
-            .then(() => {
-                this.client.database.ok = true
-                console.log('Connection was made to the database!')
-            })
-            .catch(e => {
-                console.log(e.message);
-                this.client.database = null
-            });
-    }
-
-}
+	initializeDatabase(DBWrapper, options = {}) {
+		this.client.database = new DBWrapper(options);
+		this.client.database.ok = false;
+		this.client.database
+			.startConnection()
+			.then(() => {
+				this.client.database.ok = true;
+				console.log("Connection was made to the database!");
+			})
+			.catch(e => {
+				console.log(e.message);
+				this.client.database = null;
+			});
+	}
+};
