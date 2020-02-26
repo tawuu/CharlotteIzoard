b3a476a37210b9da44b4b27f6722ea13c249b76a
Richard <richardmartinsrsms@gmail.com>
2020-02-26 11:31:55 -0300

    Trocar a collection do djs para a do Eris

--- src/loaders/APISLoaders.js
+++ src/loaders/APISLoaders.js
@@ -1,28 +1,25 @@
-const { Collection } = require('discord.js');
-const chalk = require('chalk')
-const apis = require("../apis")
+const chalk = require("chalk");
+const apis = require("../apis");
 
-module.exports = class EventLoader  {
-    constructor(client) {
-        this.client = client;
-    }
+module.exports = class EventLoader {
+	constructor(client) {
+		this.client = client;
+	}
 
-    load () {
-        try {
-            console.log(chalk.green("Apis are initializing"))
-            this.initializeApis()
-            return true
-        } catch (err) {
-            console.error(err)
-        }
-    }
-    
+	load() {
+		try {
+			console.log(chalk.green("Apis are initializing"));
+			this.initializeApis();
+			return true;
+		} catch (err) {
+			console.error(err);
+		}
+	}
 
-    initializeApis() {
-        this.client.apis = {}
-        for (let api in apis) {
-            this.client.apis[api] = apis[api]
-        }
-    }
-
-}
+	initializeApis() {
+		this.client.apis = {};
+		for (let api in apis) {
+			this.client.apis[api] = apis[api];
+		}
+	}
+};
